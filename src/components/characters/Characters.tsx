1d67caa7767f56f8d69b5b959718e4270a1115ac
Juan Montaña <juanmontana1398@gmail.com>
2022-09-04 01:02:08 -0500

    Create apiStatus to validate the request status the whole time and be able to show to the user relevant informations depending on the status

--- src/components/characters/Characters.tsx
+++ src/components/characters/Characters.tsx
@@ -2,26 +2,58 @@ import React, { useEffect, useState } from 'react';
 
 import { fetchAllCharacters } from '../../api/characters/characters';
 import { CharacterData } from '../../api/characters/character_types';
+import { ERROR, IDLE, PENDING, SUCCESS } from '../../api/constants/apiStatus';
+import { useApiStatus } from '../../api/hooks/useApiStatus';
+import { withAsync } from '../../helpers/withAsync';
 
 const useFetchAllCharacters = () => {
   const [characters, setCharacters] = useState<CharacterData[]>()
+
+  const {
+    status: fetchAllCharactersStatus,
+    setStatus: setFetchAllCharactersStatus,
+    isIdle: isFetchAllCharactersStatusIdle,
+    isPending:  isFetchAllCharactersPending,
+    isError:  isFetchAllCharactersError,
+    isSuccess:  isFetchAllCharactersSuccess,
+  } = useApiStatus(IDLE)
+
   
   const initFetchAllCharacters =  React.useCallback(
     async () => {
-      const { data } = await fetchAllCharacters()
-      setCharacters(data.results);
-    },[]
+      setFetchAllCharactersStatus(PENDING)
+      const { response, error } = await withAsync(() => fetchAllCharacters());
+
+      if(error){
+        setFetchAllCharactersStatus(ERROR);
+      }else{
+        setFetchAllCharactersStatus(SUCCESS);
+        setCharacters(response?.data.results);
+      }
+
+    },[setFetchAllCharactersStatus]
   );
 
   return {
     characters,
     initFetchAllCharacters,
+    fetchAllCharactersStatus,
+    isFetchAllCharactersStatusIdle,
+    isFetchAllCharactersPending,
+    isFetchAllCharactersError,
+    isFetchAllCharactersSuccess
   }
 }
 
 const Characters = () => {
 
-  const { characters, initFetchAllCharacters} = useFetchAllCharacters();
+  const { 
+    characters, 
+    initFetchAllCharacters, 
+    isFetchAllCharactersPending,
+    isFetchAllCharactersError,
+    isFetchAllCharactersSuccess,
+  } = useFetchAllCharacters();
 
   useEffect(()  => {
     initFetchAllCharacters();
@@ -30,7 +62,11 @@ const Characters = () => {
   return (
     <>
       <span>Character's Page</span>
-      <span>{JSON.stringify(characters)}</span>
+      { isFetchAllCharactersError && "Error fetching characters"}
+      { isFetchAllCharactersPending && "Pending fetching characters"}
+      { isFetchAllCharactersSuccess && 
+        <span>{JSON.stringify(characters)}</span>
+      }
     </>
   )
 }
